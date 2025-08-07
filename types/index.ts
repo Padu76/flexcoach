9870aa14a961b77542d17b6819466369d279390a
Padu76 <andrea.padoan@gmail.com>
2025-08-07 22:20:40 +0200

    Add files via upload

--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,27 @@
+// types/index.ts - Tipi minimi essenziali per FlexCoach
 
+export type ExerciseType = 'squat' | 'bench-press' | 'deadlift'
+
+export interface Exercise {
+  id: ExerciseType
+  name: string
+  description: string
+  difficulty: 'Principiante' | 'Intermedio' | 'Avanzato'
+  muscleGroups: string[]
+  tips: string[]
+}
+
+// Hook types per webcam
+export interface UseCameraOptions {
+  width?: number
+  height?: number
+  facingMode?: 'user' | 'environment'
+}
+
+export interface UseCameraReturn {
+  videoRef: React.RefObject<HTMLVideoElement>
+  isStreamActive: boolean
+  error: string | null
+  startCamera: () => Promise<void>
+  stopCamera: () => void
+}
