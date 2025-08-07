c372d6f8e138d055dff08ee461bbc614dece64b4
Padu76 <andrea.padoan@gmail.com>
2025-08-08 00:27:19 +0200

    Add files via upload

--- app/exercises/squat/pre-workout/page.tsx
+++ app/exercises/squat/pre-workout/page.tsx
@@ -1,1 +1,7 @@
+// app/exercises/squat/pre-workout/page.tsx
+'use client'
+import PreWorkoutPage from '../../../pre-workout/PreWorkoutPage'
 
+export default function SquatPreWorkout() {
+  return <PreWorkoutPage exerciseType="squat" />
+}
