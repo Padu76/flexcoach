5eaebd33612a358c4d703723dc8ca4d5aaf0b7aa
Padu76 <andrea.padoan@gmail.com>
2025-08-08 00:29:15 +0200

    Add files via upload

--- app/exercises/deadlift/pre-workout/page.tsx
+++ app/exercises/deadlift/pre-workout/page.tsx
@@ -1,1 +1,7 @@
+// app/exercises/deadlift/pre-workout/page.tsx
+'use client'
+import PreWorkoutPage from '../../../pre-workout/PreWorkoutPage'
 
+export default function DeadliftPreWorkout() {
+  return <PreWorkoutPage exerciseType="deadlift" />
+}
