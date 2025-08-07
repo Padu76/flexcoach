99ba3065527f03edfd77002e4836c18bae8ff040
Padu76 <andrea.padoan@gmail.com>
2025-08-08 00:28:28 +0200

    Add files via upload

--- app/exercises/bench-press/pre-workout/page.tsx
+++ app/exercises/bench-press/pre-workout/page.tsx
@@ -1,1 +1,7 @@
+// app/exercises/bench-press/pre-workout/page.tsx
+'use client'
+import PreWorkoutPage from '../../../pre-workout/PreWorkoutPage'
 
+export default function BenchPressPreWorkout() {
+  return <PreWorkoutPage exerciseType="bench-press" />
+}
