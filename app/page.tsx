3bb15c597de3aa6ca53f41a0d06927166bd0e5a3
Padu76 <andrea.padoan@gmail.com>
2025-08-09 22:08:04 +0200

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -190,7 +190,7 @@ export default function HomePage() {
         <div 
           className="absolute inset-0 bg-cover bg-center"
           style={{
-            backgroundImage: `url('https://images.unsplash.com/photo-1581009146145-b5ef050c2e1e?q=80&w=2070')`,
+            backgroundImage: `url('https://images.unsplash.com/photo-1541534741688-6078c6bfb5c5?q=80&w=2069')`,
             transform: `translateY(${(scrollY - 1600) * 0.3}px)`,
           }}
         >
